daf023cbdeeead9f5b248a16a6340962305b1f16
Jeremie Kull <DeltaMarine101@gmail.com>
2016-06-10 08:58:33 +1000

    ...

--- SDD.py
+++ SDD.py
@@ -240,7 +240,7 @@ def equation_out(data, add=''):
 
 # Calculate or render the value
 def calculate():
-    global rendered, formula, antialias_rendering
+    global rendered
 
     # time1 = time.clock()
 
@@ -381,8 +381,8 @@ for i in range(9):
            bg='#65c050', activebackground='#80d575', cursor='hand2', borderwidth=0).grid(row=3 - (i // 3), column=i % 3)
 
 symbols = ['+', '-', '*', '/', 'sin', 'cos', '(', ')']
-for i, v in symbols:
-    Button(buttons, text=v, font=('arial', 14), width=4, height=2,
+for i in range(len(symbols)):
+    Button(buttons, text=symbols[i], font=('arial', 14), width=4, height=2,
            command=lambda j=i: add_formula(symbols[j]),
            bg='#65c050', activebackground='#80d575', cursor='hand2', borderwidth=0).grid(row=i // 2 + 1,
                                                                                          column=i % 2 + 3)
